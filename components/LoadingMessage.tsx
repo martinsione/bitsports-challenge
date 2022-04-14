7e4854f713b3cf6614d764011f86e5a5c96bc8c7
Martin Sione <martinsione03@gmail.com>
2022-04-14 18:33:25 -0300

    Add img to loader component

--- components/LoadingMessage.tsx
+++ components/LoadingMessage.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+
 export default function LoadingMessage() {
   return (
-    <div className="my-6 flex justify-center">
+    <div className="my-6 flex items-center justify-center gap-2">
+      <Image alt="loading gif" height="16" src="/img/loader.gif" width="16" />
       <p className="text-[17px] font-bold text-[#828282]">Loading</p>
     </div>
   );
